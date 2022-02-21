68c4a8634c213d3be0641c5bcf09a28ea7ed2a04
Julian Schneider <92728359+Julian1612@users.noreply.github.com>
2022-02-21 14:18:49 +0100

    finished Hangman

--- Aufgaben/Hangman.cpp
+++ Aufgaben/Hangman.cpp
@@ -7,21 +7,43 @@ int main (int argc, const char * argv[])
 {
     string word;
     string searched;
+    char input;
     int i;
+    int trys;
 
+    i = 0;
+    trys = 10;
     cout << "Enter a Word: ";
     cin >> word;
     cout << "The following word is searched for: ";
-    i = 0;
     while (i < word.length())
     {
         searched.append("_");
         i++;
     }
-    cout << searched;
-    while (searched.find("_") != std::string::npos)
+    cout << searched << endl;
+    while (searched.find("_") != std::string::npos && trys > 0)
     {
-       
+        cin >> input;
+        for (int j = 0; j < word.length(); j++)
+        {
+            if(word[j] == input)
+                searched[j] = input;
+        }
+        if (searched.find(input) == std::string::npos)
+        {
+            trys --;
+        }
+        cout << "The following word is searched for: " << searched << " " << trys << " trys left" << endl;
+    }
+
+    if (trys == 0)
+    {
+        cout << "GAME OVER" << endl;
+    }
+    else
+    {
+        cout << "\nWin, the correct Word is " << word << " you got " << trys << " trys left" << endl;
     }
     return (0);
 }
