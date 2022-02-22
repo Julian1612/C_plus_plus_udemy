ee0eb7cb6fb921d56f6340df958452f26c175800
Julian Schneider <92728359+Julian1612@users.noreply.github.com>
2022-02-22 09:13:57 +0100

    changed foldernmae and finished max_min

--- Exercises/max_min.cpp
+++ Exercises/max_min.cpp
@@ -1,31 +1,34 @@
 #include <iostream>
 using namespace std;
 
-int maximum_0(int a, int b)
+int maximum(int a, int b)
 {
-    if (a > b)
+    cout << "Option 1" << endl;
+    if (a >= b)
         return (a);
     else
         return (b);
 }
 
-int maximum_1(int a, int b, int c)
+int maximum(int a, int b, int c)
 {
-    if (a > b && a > c)
+    cout << "Option 2" << endl;
+    if (a >= b && a >= c)
         return (a);
-    else if (b > a && b > c)
+    else if (b >= a && b >= c)
         return (b);
     else
         return (c);
 }
 
-int maximum_2(int a, int b, int c, int d)
+int maximum(int a, int b, int c, int d)
 {
-    if (a > b && a > c && a > d)
+    cout << "Option 3" << endl;
+    if (a >= b && a >= c && a >= d)
         return (a);
-    else if (b > a && b > c && b > d)
+    else if (b >= a && b >= c && b >= d)
         return (b);
-    else if (c > a && c > b && c > d)
+    else if (c >= a && c >= b && c >= d)
         return (c);
     else
         return (d);
@@ -41,13 +44,13 @@ int main (int argc, const char * argv[])
     int f;
     int g;
 
-    a = 2001;
-    b = 2222;
-    c = 2223;
-    d = 9999;
-    e = maximum_0(a, b);
-    f = maximum_1(a, b, c);
-    g = maximum_2(a, b, c, d);
+    a = 334;
+    b = 22222;
+    c = 221;
+    d = 92222999;
+    e = maximum(a, b);
+    f = maximum(a, b, c);
+    g = maximum(a, b, c, d);
     cout << "Das Maximum der Zahlen " << a << " und " << b << " ist: " << e << endl; 
     cout << "Das Maximum der Zahlen " << a << ", " << b << " und " << c << " ist: " << f << endl; 
     cout << "Das Maximum der Zahlen " << a << ", " << b << ", " << c << " und " << d << " ist: " << g << endl; 
